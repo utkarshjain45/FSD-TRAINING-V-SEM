fce1c21373d3e734332e282ef2ec9f590c2df65e
utkarshjain45 <jainutkarsh096@gmail.com>
2025-11-06 16:03:47 +0530

    added props component

--- React/react-component/src/App.jsx
+++ React/react-component/src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Dashboard from './component/dashboard'
+import ListProducts from './component/ListProducts'
 
 function App() {
   return (
     <div>
-      <Dashboard />    
+      <Dashboard />  
+      <ListProducts />  
     </div>
   )
 }
